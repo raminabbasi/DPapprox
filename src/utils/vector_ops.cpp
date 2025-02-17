adcfb47ba9433f67b05bf6baf595e264dcff8e08
Ramin Abbasi Esfeden <ramin.abbasiesfeden@atlascopco.com>
2025-02-17 13:28:19 +0100

    Add copyright notice

--- src/utils/vector_ops.cpp
+++ src/utils/vector_ops.cpp
@@ -1,3 +1,12 @@
+/* This file is part of DPapprox
+ * Copyright (C) 2025 Ramin Abbasi Esfeden.
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * This software is based on research described in:
+ * "A Dynamic Programming-Inspired Approach for Mixed Integer Optimal Control Problems with Dwell Time Constraints,"
+ * by Ramin Abbasi Esfeden, Christoph Plate, Sebastian Sager, Jan Swevers, 2025.
+ */
+
 #include "vector_ops.h"
 #include <stdexcept>
 
